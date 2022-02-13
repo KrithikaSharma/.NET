65eea6c984b0f5623b618bf398b4e098343f52d6
KrithikaSharma <krithikasharma2129@gmail.com>
2022-02-14 01:06:55 +0530

    Added screenshots for easy evaluation

---
+++ SQL/Assignment 2/Assignment2.sql
@@ -0,0 +1,84 @@
+create database ZensarDB
+use ZensarDB
+
+-- creating tables in ZensarDB 
+create table EMP(empno int primary key, ename varchar(20) not null, job varchar(20) not null, mgr int, hiredate varchar(15) not null, sal int not null, comm int, deptno int not null) 
+create table DEPT(deptno int, dname varchar(15), loc varchar(20)) 
+ 
+-- inserting values  
+insert into EMP 
+values(7369,'smith','clerk',7902,'17-dec-80',800,null,20), 
+(7499,'allen','salesman',7698,'20-feb-81',1600,300,30), 
+(7521,'ward','salesman',7698,'22-feb-81',1250,500,30), 
+(7566,'jones','manager',7839,'02-apr-81',2975,null,20), 
+(7654,'martin','salesman',7698,'28-sep-81',1250,1400,30), 
+(7698,'BLAKE','MANAGER',7839,'01-MAY-81',2850,null,30), 
+(7782,'CLARK','MANAGER',7839,'09-JUN-81',2450,null,10), 
+(7788,'SCOTT','ANALYST',7566,'19-APR-87',3000,null,20), 
+(7839,'KING','PRESIDENT',null,'17-NOV-81',5000,null,10), 
+(7844,'TURNER','SALESMAN',7698,'08-SEP-81',1500,0,30), 
+(7876,'ADAMS','CLERK',7788,'23-MAY-87',1100,null,20), 
+(7900,'JAMES','CLERK',7698,'03-DEC-81',950,null,30), 
+(7902,'FORD','ANALYST',7566,'03-DEC-81',3000,null,20), 
+(7934,'MILLER','CLERK',7782,'23-JAN-82',1300,null,10) 
+ 
+insert into DEPT 
+values(10,'ACCOUNTING','NEW YORK') , 
+(20,'RESEARCH','DALLAS'), 
+(30,'SALES','CHICAGO'), 
+(40,'OPERATIONS','BOSTON ') 
+ 
+-- showing values 
+select * from EMP 
+select * from DEPT 
+ 
+ 
+-- tasks 
+-- 1.Retrieve a list of MANAGERS. 
+select distinct(mgr) from emp
+
+-- 2. Find out salary of both MILLER and SMITH.
+select ename,sal from emp where ename='miller' or ename='smith'
+
+-- 3. Find out the names and salaries of all employees earning more than 1000 per month. 
+select ename,sal from EMP where sal>1000
+
+-- 4. Display the names and salaries of all employees except JAMES. 
+select ename,sal from EMP where ename!='james'
+
+-- 5. Find out the details of employees whose names begin with ‘S’. 
+select * from EMP where ename like 's%'
+
+-- 6. Find out the names of all employees that have ‘A’ anywhere in their name.
+select * from EMP where ename like '%a%'
+
+-- 7. Find out the names of all employees that have ‘L’ as their third character in their name. 
+select ename from EMP where ename like '__l%'
+
+-- 8. Find out the names of the employees whose name begin with ‘A’ or ‘M’. 
+select ename from EMP where ename like'a%' or ename like 'm%'
+
+-- 9. Compute yearly salary of SMITH. 
+select ename,sal*12 as Yearly_Salary from EMP where ename='smith'
+
+-- 10. Compute daily salary of JONES. (assuming given is monthly salary)
+select ename,sal/28 from emp as Daily_Salary where ename='jones'
+
+-- 11. Calculate the total monthly salary of all employees.
+--given salary is monthly or daily....????if daily then
+select  ename,sal*28 as monthly_salary from emp
+
+-- 12. Print the average annual salary. 
+select avg(sal*365) as avg_annual_salary from emp
+
+-- 13. Select the name, job, salary, department number of all employees except SALESMAN from department number 30. 
+select ename,job,sal,deptno from EMP where job not like 'salesman' and deptno=30
+
+-- 14. List unique departments of the EMP table. 
+select distinct(d.dname) from DEPT d,EMP e where d.deptno=e.deptno 
+
+-- 15. List the name and salary of employees who earn more than 1500 and are in department 10 or 30. Label the columns Employee and Monthly Salary respectively.
+select ename as Employee,sal as 'Monthly Salary' from EMP where ((sal>1500) and (deptno=10 or deptno=30))
+
+
+
---
+++ SQL/Special Assignment/SPCL_Assignment.sql
@@ -0,0 +1,77 @@
+-- showing values 
+select * from EMP 
+select * from DEPT
+
+-- creating tables
+create table Clients1(Client_ID int primary key, Cname varchar(40) not null, CAddress varchar(30),Email varchar(30) unique, Phone int, Business varchar(20) not null) 
+create table Departments(Deptno int primary key,Dname varchar(15) not null,Loc varchar(20))
+create table Employees(Empno int primary key, Ename varchar(20) not null, job varchar(15),Salary int,Deptno int foreign key references Departments(Deptno),check (Salary>0))
+
+create table Projects(Project_ID int primary key, Descr varchar(30) not null,Start_date date, Planned_End_Date date, Actual_End_Date date,Budget int,Client_ID int foreign key references Clients(Client_ID), check (Actual_End_Date>Planned_End_date))
+create table EmpProjectTasks(Project_ID int,Empno int, Start_Date date,End_Date date,Task varchar(25) not null,Status varchar(15) not null,primary key(project_ID,Empno))
+--foreign key(Project_ID,empno) references (Employees)    - not able to add refernce of 2 tables
+
+-- inserting data
+insert into Projects
+values(401,'Inventory','01-Apr-11','01-Oct-11','31-Oct-11',150000,1001),
+(402,'Accounting','01-Aug-11','01-Jan-12',null,500000,1002),
+(403,'Payroll','01-Oct-11','31-Dec-11',null,75000,1003),
+(404,'Contact Mgmt','01-Nov-11','31-Dec-11',null,50000,1004)
+
+
+insert into Clients1
+values(1001,'ACME Utilities','Noida','contact@acmeutil.com',9567880032,'Manufacturing'),
+(1002,'Trackon Consultants','Mumbai','consult@trackon.com',8734210090,'Consultant'),
+(1003,'MoneySaver Distributors'	,'Kolkata','save@moneysaver.com',7799886655,'Reseller'),
+(1004,'Lawful Corp','Chennai','justice@lawful.com',9210342219,'Professional')
+
+insert into Departments
+values(10,'Design','Pune'),
+(20,'Developmet','Pune'),
+(30,'Testing','Mumbai'),
+(40,'Document','Mumbai')
+
+
+insert into Employees
+values(7001,'Sandeep','Analyst',25000,10),
+(7002,'Rajesh','Designer',30000,10),
+(7003,'Madhav','Developer',40000,20),
+(7004,'Manoj','Developer',40000,20),
+(7005,'Abhay','Designer',35000,10),
+(7006,'Uma','Tester',30000,30),
+(7007,'Gita','Tech. Writer',30000,40),
+(7008,'Priya','Tester',35000,30),
+(7009,'Nutan','Developer',45000,20),
+(7010,'Smita','Analyst',20000,10),
+(7011,'Anand','Project Mgr',65000,10)
+
+insert into EmpProjectTasks
+values(401,7001,'01-Apr-11','20-Apr-11','System Analysis','Completed'),
+(401,7002,'21-Apr-11','30-May-11','System Design','Completed'),
+(401,7003,'01-Jun-11','15-Jul-11','Coding','Completed'),
+(401,7004,'18-Jul-11','01-Sep-11','Coding','Completed'),
+(401,7006,'03-Sep-11','15-Sep-11','Testing','Completed'),
+(401,7009,'18-Sep-11','05-Oct-11','Code Change','Completed'),
+(401,7008,'06-Oct-11','16-Oct-11','Testing','Completed'),
+(401,7007,'06-Oct-11','22-Oct-11','Documentation','Completed'),
+(401,7011,'22-Oct-11','31-Oct-11','Sign off','Completed'),
+(402,7010,'01-Aug-11','20-Aug-11','System Analysis','Completed'),
+(402,7002,'22-Aug-11','30-Sep-11','System Design','Completed'),
+(402,7004,'01-Oct-11',null,'Coding','In Progress')
+
+-- select stmts
+select * from Clients1
+select * from Departments
+select * from Employees
+select * from Projects
+select * from EmpProjectTasks
+
+-- help
+sp_help Clients1
+sp_help Departments
+sp_help Employees
+sp_help Projects
+sp_help EmpProjectTasks
+
+
+
