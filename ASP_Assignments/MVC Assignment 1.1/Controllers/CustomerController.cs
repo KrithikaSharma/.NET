b4d2adfcf20f3a7f03f13d7b7177a97817bbc877
KrithikaSharma <krithikasharma2129@gmail.com>
2022-03-17 23:12:42 +0530

    Contains MVC Ass1.1 Customer based assignment

---
+++ ASP_Assignments/MVC Assignment 1.1/Controllers/CustomerController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Assign.Models;
+
+namespace MVC_Assign.Controllers
+{
+    public class CustomerController : Controller
+    {
+        NorthwindEntities1 Northwinddb = new NorthwindEntities1();
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetCustomerByCountry()
+        {
+            List<string> custname = (from cust in Northwinddb.Customers
+                                     where cust.Country == "Germany"
+                                     select cust.ContactName).ToList();
+            return View(custname);
+        }
+
+        public ActionResult GetCustomerDetailsById()
+        {
+            List<Customer> customer = (from cust in Northwinddb.Customers
+                                       join o in Northwinddb.Orders
+                                       on cust.CustomerID equals o.CustomerID
+                                       where o.OrderID == 10248
+                                       select cust).ToList();
+
+            return View(customer);
+        }
+    }
+}
